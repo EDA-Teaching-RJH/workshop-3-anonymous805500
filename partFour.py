e823f9404de88863329f609faa1976d8c8468452
anonymous805500 <mm2452@kent.ac.uk>
2024-10-10 16:08:27 +0000

    P4

--- partFour.py
+++ partFour.py
@@ -1,1 +1,6 @@
-
+username = input("Uname ")
+password = input("Pword ")
+if username.lower() == "admin" and password.lower() == "password123":
+    print("Welcome")
+else:
+    print("access denied")
