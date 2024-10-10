edfb97ed78b325f556fc52a1a4cc5c02c5692e5d
anonymous805500 <mm2452@kent.ac.uk>
2024-10-10 15:41:46 +0000

    P2

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,10 @@
-
+import random
+secret_number = random.randint(1 , 10)
+x = int(input("Please enter "))
+if x >= secret_number:
+   print("Too High")
+if x <= secret_number:
+   print("Too Low")  
+if x == secret_number:
+   print("Correct")
+print(secret_number)
