9902a3778ab4b125a67f19b204a620eb65ea6c91
anonymous805500 <mm2452@kent.ac.uk>
2024-10-10 16:27:52 +0000

    P6

--- partSix.py
+++ partSix.py
@@ -1,1 +1,9 @@
-
+age = int(input("Enter age "))
+student = input("Are you a student ? Yes/No")
+if age < 12 or age >= 65:
+    print("£5")
+elif student.lower() == "yes":
+    print("£8")
+else:
+    print("£10")
+    
